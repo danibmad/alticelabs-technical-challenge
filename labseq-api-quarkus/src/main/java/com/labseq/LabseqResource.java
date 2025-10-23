65c407c098003d9f80cb9b05f87166222b5e39b3
Daniel Madaíl <danielmadail@msn.com>
2025-10-23 17:19:44 +0100

    added endpoint swagger documentation

--- labseq-api-quarkus/src/main/java/com/labseq/LabseqResource.java
+++ labseq-api-quarkus/src/main/java/com/labseq/LabseqResource.java
@@ -1,5 +1,11 @@
 package com.labseq;
 
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.parameters.Parameter;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
@@ -7,6 +13,7 @@ import jakarta.ws.rs.PathParam;
 import java.math.BigInteger;
 
 @Path("/labseq")
+@Tag(name = "LabSeq API", description = "Compute labseq sequence values")
 public class LabseqResource {
     
     @Inject
@@ -14,7 +21,30 @@ public class LabseqResource {
 
     @GET
     @Path("/{n}")
-    public String getLabseq(@PathParam("n") long n)
+    @Operation(
+        summary = "Get labseq value",
+        description = "Returns the labseq sequence value for index n"
+    )
+    @APIResponse(
+        responseCode = "200",
+        description = "Returns the calculation for the labseq sequence of the n index",
+        content = @Content(schema = @Schema(implementation = String.class))
+    )
+    @APIResponse(
+        responseCode = "400",
+        description = "Invalid input in case of parameter n < 0"
+    )
+    @APIResponse(
+        responseCode = "404",
+        description = "Not found in case of parameter n not being a number"
+    )
+    public String getLabseq(
+         @Parameter(
+            description = "A non negative integer",
+            example = "10",
+            required = true
+    )
+    @PathParam("n") long n)
     {
         BigInteger result = service.calculateLabseq(n);
         return result.toString();
